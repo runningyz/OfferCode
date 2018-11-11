8fe9545ee6c35ab393009ce419fcf8d44c833a09
runningyz <runningyangzhi@gmail.com>
2018-11-11 15:52:38 +0800

    [add]add 69

--- src/main/java/Sqrt_69/Sqrt.java
+++ src/main/java/Sqrt_69/Sqrt.java
@@ -7,7 +7,7 @@ public class Sqrt {
         int right = x;
         while(true){
             int mid = (left + right) / 2;
-            if(mid > x/mid) right = mid -1; //此处不用mid*mid<x作为条件原因是,如果mid特别大,乘积超出int范围,则会变窄处理,只保留低32位 会造成死循环
+            if(mid > x/mid) right = mid -1; // 此处不用mid*mid<x作为条件原因是,如果mid特别大,乘积超出int范围,则会变窄处理,只保留低32位 会造成死循环
             else{
                 if(mid+1 > x/(mid+1)) return mid;
                 left = mid + 1;
